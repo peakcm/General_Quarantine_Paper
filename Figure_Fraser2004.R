f3cc67b078156ac019785a09c5221646ba09775c
peakcm <coreypeak@gmail.com>
2015-11-01 07:55:33 +0100

    Create new R file for the code to create the Fraser 2004 figure using the output from the doomsday code.

---
+++ Figure_Fraser2004.R
@@ -0,0 +1,110 @@
+# Figure_Fraser2004
+
+#### Load libraries ####
+library(ggplot2)
+
+#### Load Rdata ####
+# Load the data workspace from the Figure_Doomsday.R file
+load(paste("~/Dropbox/Ebola/General_Quarantine_Paper/General_Quarantine_Paper/20151024_Ebola_PlotDoomsday/20151024_Ebola_doomsday_2.RData", sep=""))
+
+#### Calculate Fraser 2004 Theta value ####
+# Theta is the proprtion of infections that occur prior to symptom onset
+data.dooms
+day$theta <- NA
+
+for (i in 1:nrow(data.doomsday)){
+  a <- 0
+  b <- data.doomsday[i, "d_inf"]
+  c <- data.doomsday[i, "pi_t_triangle_center"] * data.doomsday[i, "d_inf"]
+  x <- -(data.doomsday[i, "T_lat_offset"])   # Time of symptom onset
+  
+  if (data.doomsday[i, "T_lat_offset"] >= 0){   # If infectious after symptom onset
+    data.doomsday[i, "theta"] = 0
+  } else if (b <= x){   # If end of infectiousness is before symptom onset
+    data.doomsday[i, "theta"] = 1
+  } else {
+    if (c <= x){   # If the peak occurs before onset of symptoms
+      # Calculate the right hand side triangle
+      height <- 2*(b-x) / ((b-a)*(b-c))
+      base <- b-x
+      rhs <- 0.5*base*height
+      data.doomsday[i, "theta"] <- 1-rhs
+    }
+    if (c > x){
+      # Calculate the left hand side triangle
+      height <- 2*(x-a) / ((b-a)*(c-a))
+      base <- x
+      lhs <- 0.5*base*height
+      data.doomsday[i, "theta"] <- lhs
+    }
+  }
+}
+
+#### Fraser 2004 Plot ####
+plot_doomsday <- ggplot(data.doomsday) +
+  geom_point(data = data.doomsday[data.doomsday$R_q < 1,], aes(theta, R_0), color = "cornflowerblue", size = 3) +
+  geom_point(data = data.doomsday[data.doomsday$R_s < 1,], aes(theta, R_0), color = "darkgoldenrod", size = 3) +  
+  geom_point(data = data.doomsday[data.doomsday$R_hsb < 1,], aes(theta, R_0), color = "mediumturquoise", size = 3) +
+  theme_bw() + xlim(c(0,1)) +
+  ylim(c(0, max(data.doomsday$R_0)))
+plot_doomsday
+
+#### Zero adapted Fraser 2004 plot ####
+# Need some way of showing what's happening in the theta = 0. Need to show the proportion of trials where there was control.
+# Make a heatmap with the minimum sufficient control method for each grid space of theta and R_0. 
+# So, the theta=1 colum may be green first, then brown, then blue, then nothing when no intervention is sufficient.
+# Set the threshold to 90% of trials, there is control.
+
+# Find max R_0 that HSB maintains 90% control
+layout(c(1))
+plot(data.doomsday[data.doomsday$theta == 0 & data.doomsday$R_hsb > 1, "R_0"], col="red")
+points(data.doomsday[data.doomsday$theta == 0 & data.doomsday$R_hsb < 1, "R_0"], col="green", add = "TRUE")
+
+layout(cbind(c(1,2)))
+no_control <- hist(data.doomsday[data.doomsday$theta == 0 & data.doomsday$R_hsb >= 1, "R_0"], breaks = seq(1, 10, 0.2))
+control <- hist(data.doomsday[data.doomsday$theta == 0 & data.doomsday$R_hsb < 1, "R_0"], breaks = seq(1, 10, 0.2))
+
+control$density / (control$density + no_control$density)
+HSB_max <- control$breaks[max(which(control$density / (control$density + no_control$density) > 0.90))]
+
+# Find max R_0 that SM maintains 90% control
+plot(data.doomsday[data.doomsday$theta == 0 & data.doomsday$R_s > 1, "R_0"], col="red")
+points(data.doomsday[data.doomsday$theta == 0 & data.doomsday$R_s < 1, "R_0"], col="green", add = "TRUE")
+
+layout(cbind(c(1,2)))
+no_control <- hist(data.doomsday[data.doomsday$theta == 0 & data.doomsday$R_s >= 1, "R_0"], breaks = seq(1, 10, 0.2))
+control <- hist(data.doomsday[data.doomsday$theta == 0 & data.doomsday$R_s < 1, "R_0"], breaks = seq(1, 10, 0.2))
+
+control$density / (control$density + no_control$density)
+S_max <- control$breaks[max(which(control$density / (control$density + no_control$density) > 0.90))]
+
+# Find max R_0 that Q maintains 90% control
+plot(data.doomsday[data.doomsday$theta == 0 & data.doomsday$R_q > 1, "R_0"], col="red")
+points(data.doomsday[data.doomsday$theta == 0 & data.doomsday$R_q < 1, "R_0"], col="green", add = "TRUE")
+
+layout(cbind(c(1,2)))
+no_control <- hist(data.doomsday[data.doomsday$theta == 0 & data.doomsday$R_q >= 1, "R_0"], breaks = seq(1, 10, 0.2))
+control <- hist(data.doomsday[data.doomsday$theta == 0 & data.doomsday$R_q < 1, "R_0"], breaks = seq(1, 10, 0.2))
+
+control$density / (control$density + no_control$density)
+Q_max <- control$breaks[max(which(control$density / (control$density + no_control$density) > 0.90))]
+
+# Plot
+plot_fraser <- ggplot() +
+  geom_point(data = data.doomsday[data.doomsday$R_q < 1 & data.doomsday$R_0 < Q_max & data.doomsday$theta == 0,],  aes(theta-0.02, R_0), color = "cornflowerblue", size = 3) +
+  geom_point(data = data.doomsday[data.doomsday$R_q < 1 & data.doomsday$theta > 0,], aes(theta, R_0), color = "cornflowerblue", size = 3) +
+  geom_point(data = data.doomsday[data.doomsday$R_s < 1 & data.doomsday$R_0 < S_max & data.doomsday$theta == 0,],  aes(theta-0.01, R_0), color = "darkgoldenrod", size = 3) +
+  geom_point(data = data.doomsday[data.doomsday$R_s < 1 & data.doomsday$theta > 0,], aes(theta, R_0), color = "darkgoldenrod", size = 3) +  
+  geom_point(data = data.doomsday[data.doomsday$R_hsb < 1 & data.doomsday$R_0 < HSB_max & data.doomsday$theta == 0,],  aes(theta, R_0), color = "mediumturquoise", size = 3) +
+  geom_point(data = data.doomsday[data.doomsday$R_hsb < 1 & data.doomsday$theta > 0,], aes(theta, R_0), color = "mediumturquoise", size = 3) +
+  theme_bw() + xlim(c(-0.02,1)) + xlab ("Theta = Proportion of infections that occur prior to\nsymptoms or by asymptomatic infection") +
+  ylim(c(1, Q_max+0.5))
+plot_fraser
+
+pdf(file=paste("~/Dropbox/Ebola/General_Quarantine_Paper/General_Quarantine_Paper/", root, "_PlotFraser.pdf", sep=""))
+plot(plot_fraser)
+dev.off()
+
+#### Save ####
+save.image(paste("~/Dropbox/Ebola/General_Quarantine_Paper/General_Quarantine_Paper/", root, "_fraser.RData", sep=""))
+
